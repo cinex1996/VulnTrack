adb286386ce6f74c1d036cd359680e4bc7e92357
cinex1996 <mskarzynski8@gmail.com>
2026-06-08 03:11:03 +0200

    I added tests

--- notifications/urls.py
+++ notifications/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from notifications import views
 
 urlpatterns = [
-    path('notifications/', views.notification_view, name='notifications'),
-    path('notifications/<int:pk>/read/', views.mark_as_read, name="notification_read"),
+    path('', views.notification_view, name='notifications'),
+    path('<int:pk>/read/', views.mark_as_read, name="notification_read"),
+    path('mark-all-read/', views.mark_all_as_read, name="mark_all_as_read"),
 ]
--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from projects.models import Project
 
-# Create your tests here.
+class ProjectModelTest(TestCase):
+    def test_str(self):
+        project = Project(name="Test")
+        self.assertEqual(str(project), "Test")
+
+    def test_is_active_default(self):
+        project = Project(name="Test")
+        self.assertTrue(project.is_active)
+
+    def test_project_list_requirements_login(self):
+        response = self.client.get('/projects/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_project_list_logged_in(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='tester', password='pass1234')
+        self.client.login(username='tester', password='pass1234')
+        response = self.client.get('/projects/')
+        self.assertEqual(response.status_code, 200)
