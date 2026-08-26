19accbac356757ca0bad9d743f78683f601caf60
cinex1996 <mskarzynski8@gmail.com>
2026-08-26 18:34:07 +0200

    Add SITE_URL in settings

--- VulnTrackAdmin/settings.py
+++ VulnTrackAdmin/settings.py
@@ -150,3 +150,4 @@ CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND', default='redis://localhost:
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+SITE_URL = env('SITE_URL', default='http://127.0.0.1:8000')
